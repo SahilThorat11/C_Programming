6ef2e70f58784638464067ce8007c299d3f362e8
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-11 12:21:01 +0530

    Count Frequency of given number from the linked list

---
+++ Assignments/Assignment_44/Program44_4.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+int Frequency(PNODE first, int iNo)
+{
+    PNODE temp = NULL;
+    int iCount = 0;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        if(temp -> data == iNo)
+        {
+            iCount++;
+        }
+
+        temp = temp -> next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iRet = 0, iValue = 0, iNode = 0, iCnt = 0, iNo = 0;
+    PNODE head = NULL;
+
+    printf("How many nodes do you want? : ");
+    scanf("%d", &iNode);
+
+    for(iCnt = 1; iCnt <= iNode; iCnt++)
+    {
+        printf("Enter number %d : ", iCnt);
+        scanf("%d", &iValue);
+        InsertFirst(&head, iValue);
+    }
+
+    Display(head);
+
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    printf("Enter the number that you want to search : ");
+    scanf("%d", &iNo);
+
+    iRet = Frequency(head, iNo);
+    printf("ferquency of %d is : %d\n\n", iNo, iRet);
+
+    return 0;
+}

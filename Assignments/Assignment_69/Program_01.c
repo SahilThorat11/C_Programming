3f80adf61bdb8e9f84b5863077751c969ddf524f
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-09 12:18:46 +0530

    Recursive code to display * on Screen

---
+++ Assignments/Assignment_69/Program_01.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    static int i = 0;
+
+    if(i < iNo)
+    {
+        printf("*\t");
+        i++;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}

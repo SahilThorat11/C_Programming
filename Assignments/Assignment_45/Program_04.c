b12d189270b2486b6f5a8a888f88d3003bc76394
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-11 21:10:23 +0530

    Function to Count greater number number from given

--- Assignments/Assignment_45/Program_04.c
+++ Assignments/Assignment_45/Program_04.c
@@ -5,20 +5,20 @@
 struct node
 {
     int data;
-    struct node *next;
+    struct node * next;
 };
 
 typedef struct node NODE;
-typedef struct node *PNODE;
-typedef struct node **PPNODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
 
-void InsertFirst(PPNODE first, int No)
+void InsertFirst(PPNODE first, int iNO)
 {
     PNODE newn = NULL;
 
     newn = (PNODE)malloc(sizeof(NODE));
 
-    newn -> data = No;
+    newn -> data = iNO;
     newn -> next = NULL;
 
     if(*first == NULL)
@@ -32,7 +32,7 @@ void InsertFirst(PPNODE first, int No)
     }
 }
 
-void Dispaly(PNODE first)
+void Display(PNODE first)
 {
     PNODE temp = NULL;
 
@@ -52,7 +52,6 @@ void Dispaly(PNODE first)
 int Count(PNODE first)
 {
     int iCount = 0;
-
     PNODE temp = NULL;
 
     temp = first;
@@ -62,62 +61,47 @@ int Count(PNODE first)
         iCount++;
         temp = temp -> next;
     }
+    
+    return iCount;
+}
+
+int CountGreater(PNODE first, int iNo)
+{
+    PNODE temp = first;
+    int iCount = 0;
+
+    while(temp != NULL)
+    {
+        if(temp->data > iNo)
+        {
+            iCount++;
+        }
+        temp = temp->next;
+    }
 
     return iCount;
 }
 
-void DisplayPrime(PNODE first)
-{
-    PNODE temp = first;
-    int iNo = 0, iCnt = 0;
-    bool bPrime = false;
-
-    while(temp != NULL)
-    {
-        iNo = temp->data;
-        bPrime = true;
-
-        if(iNo <= 1)
-        {
-            bPrime = false;
-        }
-
-        for(iCnt = 2; iCnt <= iNo / 2; iCnt++)
-        {
-            if(iNo % iCnt == 0)
-            {
-                bPrime = false;
-                break;
-            }
-        }
-
-        if(bPrime == true)
-        {
-            printf("| %d | -> ", iNo);
-        }
-
-        temp = temp->next;
-    }
-
-    printf("NULL\n");
-}
-
 int main()
 {
-    int iRet = 0, iValue = 0;
-    PNODE head = NULL; 
+    int iRet = 0, iValue;
+    PNODE head = NULL;
 
-    InsertFirst(&head, 111);
-    InsertFirst(&head, 101);
+    InsertFirst(&head, 75);
+    InsertFirst(&head, 11);
     InsertFirst(&head, 51);
-    InsertFirst(&head, 21);
+    InsertFirst(&head, 22);
     InsertFirst(&head, 11);
 
-    Dispaly(head);
+    Display(head);
     iRet = Count(head);
-    printf("Number of nodes in linkedlist are : %d\n", iRet);
+    printf("Number of nodes are : %d\n\n", iRet);
 
-    DisplayPrime(head);
+    printf("Enter the number : ");
+    scanf("%d", &iValue);
+
+    iRet = CountGreater(head, iValue);
+    printf("Numbers greater than %d are : %d", iValue, iRet);
 
     return 0;
 }
